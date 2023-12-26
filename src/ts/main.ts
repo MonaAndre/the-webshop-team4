bb9b82b9629cc339aea5b6d25d45c9e152aeacd3
Carolina Ekdal <Carolina.ekdal@gmail.com>
2023-12-26 22:45:07 +0100

    Changed addEventlistener and some more related scss

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,6 @@
 import "./../scss/style.scss";
+import { hamburger } from "./hamburgerMenu";
+
+const menuBtn = document.querySelector(".hamburger") as HTMLButtonElement;
+
+menuBtn.addEventListener("click", hamburger);
