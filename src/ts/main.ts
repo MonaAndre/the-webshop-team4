a0074a26665273d8cb54fa773f49ea7d14d51ea9
Carolina Ekdal <Carolina.ekdal@gmail.com>
2023-12-27 21:38:47 +0100

    fixed banner & z-index

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,8 +3,10 @@ import "./../scss/style.scss";
 //----------------Hamburger Menu Functions-----------------------
 const menuBtn = document.querySelector(".hamburger") as HTMLButtonElement;
 const hamburgerNav = document.querySelector(".hamburgerNav") as HTMLElement;
+const banner = document.querySelector(".banner") as HTMLElement;
 //Add a listener to listen after click on hamburger menu icon
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("isActive");
   hamburgerNav.classList.toggle("isActive");
+  banner.id.toggle("isActive");
 });
